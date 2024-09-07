e8e1d636d37e27288687bf387cbe849277c976fc
Kehinde Olaleye Simeon <60666942+DeEminentCoder@users.noreply.github.com>
2024-09-07 16:25:14 +0100

    Navigating Through Python

--- app.py
+++ app.py
@@ -1,3 +1,44 @@
-a = 2
-b = 3
-print(a + b)
+# #Variables
+# FavouriteNumber = 200
+# print(f"My fav number is : {FavouriteNumber}")
+
+# #string
+# greeting = "Hello, Im Simeon"
+# print(greeting)
+
+# #Boolean
+# Is_Python_fun = True
+# Is_Python_boring = False
+# print(f"is_python_fun? {Is_Python_fun}")
+
+#Control_Flow
+# temperature = 3
+# if temperature > 25:
+#     print("Its hot outside")
+# elif temperature > 15:
+#     print("Its warm outside")
+# else:
+#     print("Its cold")
+    
+    
+    #Loop
+    #For Loop (The no of time you want your code to ilterate)
+# for i in range(7):
+#     print(f"This a for loop {i}")
+    
+    #While Loop (code that runs until a condition is met)
+# CountDown = 5
+# while CountDown > 0:
+#     print(f"countdown: {CountDown}")
+#     CountDown -=1
+# print("Blast off")
+
+#Modules
+#In-built Module(comes with python package) e.g import, math,
+import math
+
+result = math.sqrt(16)
+print(f"The sqare root of 16: {result}")
+
+#Customise Module (You create them yourself) e.g
+    
